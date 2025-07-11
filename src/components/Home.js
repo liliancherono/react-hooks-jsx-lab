ce1566a24701c7652e389c0d13f914a535499f0e
Lilian Kirwa <cheronlilian21@gmail.com>
2025-07-11 23:32:50 +0300

    update the latest commits

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import { name, city } from "../data/user";
 
-function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+export default function Home() {
+
+console.log(`${name} is a Web Developer from ${city}`);
+
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
-
-export default Home;
---
+++ src/data/user.js
@@ -0,0 +1,4 @@
+// src/data/user.js
+export const name = "Lilian";
+export const city = "Nairobi";
+export const image = "https://i.imgur.com/mV8PQxj.gif";
